92479eb8c5b55c2b04fd4bedc2e3a7a0527bbfc2
kairung <kairung@webmail.npru.ac.th>
2023-11-23 19:47:10 +0700

    .commit

--- predictIris.py
+++ predictIris.py
@@ -32,4 +32,26 @@ st.markdown("")
 
 dt = pd.read_csv("./data/Root1.csv")
 st.write(dt.head(10))
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+if st.button("ทำนายผล"):
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/Root1.csv") 
+   X = dt.drop('Credit_History', axis=1)
+   y = dt.Credit_History   
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)   
+   
 
